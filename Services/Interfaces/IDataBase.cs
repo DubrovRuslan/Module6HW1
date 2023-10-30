40f3848d9339883a68db382949da8aca297c9953
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-10-30 21:35:37 +0200

    done

---
+++ Services/Interfaces/IDataBase.cs
@@ -0,0 +1,13 @@
+﻿using Module6HW1.Items;
+
+namespace Module6HW1.Services.Interfaces
+{
+    public interface IDataBase
+    {
+        void AddToList(Book Book);
+        List<Book> GetBooks();
+        void DeleteBookById(int id);
+        Book UpdateBook(Book Book, int id);
+        Book GetBook(int id);
+    }
+}
